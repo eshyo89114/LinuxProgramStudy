43051be69a2e3ba6142a270d49669c2d09ae3e64
XIAO WEI <eshyo@live.jp>
2015-10-28 19:31:20 +0900

    gcc -o output input ----> gcc input ; output=input(default)

---
+++ GccAutoName.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc,char *argv[]){
+	if(argc < 2){
+		fprintf(stderr,"%s: error number of arguments!\n",argv[0]);
+		exit(1);
+	}
+	char cmd[256]="gcc -o ";
+	char out_name[256]={0};
+//	printf("argv[1]=%s,strlen(argv[1])=%lu\n",argv[1],strlen(argv[1]));
+    strncpy(out_name,argv[1],strlen(argv[1])-2);
+	printf("\033[1;35;40m This version just only support one argment!\033[0m\n");
+	printf(" The output file is \"%s\"\n",out_name);
+	strcat(cmd,out_name);	
+	strcat(cmd," ");
+    strcat(cmd,argv[1]);
+//	printf("%s\n",cmd);
+	if(system(cmd)){
+		exit(1);
+	}
+	exit(0);
+}
