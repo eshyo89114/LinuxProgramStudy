ab77630f815fb68e3a0c5467499633ebcac5baf3
XIAO WEI <eshyo@live.jp>
2015-10-29 10:05:22 +0900

    GccAutoName version2

--- GccAutoName.c
+++ GccAutoName.c
@@ -12,13 +12,14 @@ int main(int argc,char *argv[]){
 //	printf("argv[1]=%s,strlen(argv[1])=%lu\n",argv[1],strlen(argv[1]));
     strncpy(out_name,argv[1],strlen(argv[1])-2);
 	printf("\033[1;35;40m This version just only support one argment!\033[0m\n");
-	printf(" The output file is \"%s\"\n",out_name);
 	strcat(cmd,out_name);	
 	strcat(cmd," ");
     strcat(cmd,argv[1]);
 //	printf("%s\n",cmd);
 	if(system(cmd)){
 		exit(1);
+	}else{
+		printf(" The output file is \"%s\"\n",out_name);
 	}
 	exit(0);
 }
